166bb1d7c06e58d1abf45ba65b4ae011c2b474ca
hyunkyu-Park <park620630@gmail.com>
2024-05-03 18:12:02 -0400

    first commit

---
+++ 584_Find_Customer_Referee.sql
@@ -0,0 +1,50 @@
+-- https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Table: Customer
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | referee_id  | int     |
+-- +-------------+---------+
+-- In SQL, id is the primary key column for this table.
+-- Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+-- Find the names of the customer that are not referred by the customer with id = 2.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+-- Input: 
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
+
+
+
+SELECT name
+FROM Customer
+WHERE COALESCE(referee_id, 0) != 2
+
+-- COALESCE is used to replace NULL values with zero before checking whether it is equal to 2 or not.
